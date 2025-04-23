d20f973930e387143b3d185314f94a1ba12a6171
Anukool Patel <151703654+anukool23@users.noreply.github.com>
2025-04-23 23:51:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
-
+//Root element
 const container = document.getElementById('root');
 const root = createRoot(container);
 
